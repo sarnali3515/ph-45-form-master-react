f41ee41ae88242218d4a0218bad2e0327d7aef8f
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-03-25 14:57:20 +0600

    prop drilling

--- src/Components/Cousin/Cousin.jsx
+++ src/Components/Cousin/Cousin.jsx
@@ -1,9 +1,20 @@
+import Friend from "../Friend/Friend";
+import Special from "../Special/Special";
 
-const Cousin = ({ name }) => {
+const Cousin = ({ name, asset }) => {
     return (
         <div>
             <h2>Cousin</h2>
             <p>{name}</p>
+            <section>
+                {
+                    asset && <Special asset={asset}></Special>
+
+                }
+                {
+                    name === 'Rubaiya' && <Friend></Friend>
+                }
+            </section>
         </div>
     );
 };
--- src/Components/Grandpa/Grandpa.jsx
+++ src/Components/Grandpa/Grandpa.jsx
@@ -1,19 +1,36 @@
+import { createContext, useState } from "react";
 import Aunty from "../Aunt/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css'
 
+export const AssetContext = createContext('gold');
+export const MoneyContext = createContext(1000);
+
 const Grandpa = () => {
+    const [money, setMoney] = useState(1000);
+    const asset = 'Diamond'
     return (
         <div className="grandpa">
             <h2>Grandpa</h2>
-            <section className="flex">
-                <Dad></Dad>
-                <Uncle></Uncle>
-                <Aunty></Aunty>
-            </section>
+            <p>Net Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className="flex">
+                        <Dad asset={asset}></Dad>
+                        <Uncle asset={asset}></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
 
 export default Grandpa;
+
+/**
+ * 1. Create a context and export it
+ * 2. Add provider for the context with a value, value coud be anything
+ * 3. useContext to access value in the context api
+ *  */ 
